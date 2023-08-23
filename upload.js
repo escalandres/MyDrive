881153887fc43ce7c46bc4a485752a37b2066bda
Andres Escala <79557629+escalandres@users.noreply.github.com>
2023-08-22 22:14:39 -0600

    Upload files module updated

--- upload.js
+++ upload.js
@@ -3,26 +3,65 @@ const path = require('path');
 const fs = require('fs'); // Importa el módulo fs
 // Set up storage for uploaded files
 
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     const subfolder = req.userId; // Obtener el valor del campo 'id' del JSON
+//     const destinationPath = path.join(__dirname,'ftp', subfolder);
+//     console.log('destination',destinationPath)
+//     fs.mkdir(destinationPath, { recursive: true }, (err) => {
+//         if (err) {
+//             console.error('Error al crear la carpeta:', err);
+//             return cb(err, null);
+//         }
+//         cb(null, destinationPath);
+//     });
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   }
+// });
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    console.log('hola')
-    console.log('body',req.userId)
+  destination: async (req, file, cb) => {
     const subfolder = req.userId; // Obtener el valor del campo 'id' del JSON
-    console.log(subfolder);
-    const destinationPath = path.join(__dirname,'ftp', subfolder);
-    console.log('destination',destinationPath)
+    const destinationPath = path.join(__dirname, 'ftp', subfolder);
+    console.log('destination', destinationPath);
+
     fs.mkdir(destinationPath, { recursive: true }, (err) => {
-        if (err) {
-            console.error('Error al crear la carpeta:', err);
-            return cb(err, null);
-        }
-        cb(null, destinationPath);
+      if (err) {
+        console.error('Error al crear la carpeta:', err);
+        return cb(err, null);
+      }
+      cb(null, destinationPath);
     });
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
+  filename: async (req, file, cb) => {
+    const originalName = file.originalname;
+    const destinationPath = path.join(__dirname, 'ftp', req.userId);
+
+    let newFilename = originalName;
+    let counter = 1;
+
+    while (await fileExists(path.join(destinationPath, newFilename))) {
+      const fileNameWithoutExtension = originalName.replace(/\.[^/.]+$/, '');
+      const fileExtension = path.extname(originalName);
+      newFilename = `${fileNameWithoutExtension} (${counter})${fileExtension}`;
+      counter++;
+    }
+
+    cb(null, newFilename);
+  },
 });
+
+async function fileExists(filePath) {
+  try {
+    await fs.promises.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
   
 // Create the multer instance
 const upload = multer({ storage: storage });
