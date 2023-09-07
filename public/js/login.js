eb0bff203bc39cae2a255a209c3e80d52cefc22c
Andres Escala <79557629+escalandres@users.noreply.github.com>
2023-09-06 23:09:27 -0600

    Toastify js added

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,21 @@ form.addEventListener('submit', async (event) => {
     const data = await response.json();
     if (!data.success) {
         // Mostrar un mensaje de error si el inicio de sesión falla
-        alert(data.message);
+        // alert(data.message);
+        Toastify({
+            text: "El usuario y/o contraseña no es válido",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #FF5733, #E74C3C)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
     }else{
         window.location.href = '/mydrive'
     }
